19bef200830fe818fa7c34e74e260a663a7bfc90
RAJSHEKHAR S <62935800+rajhirematha24@users.noreply.github.com>
2022-03-08 16:52:34 +0530

    Update Demo.java

--- Learn_Selenium/src/test/java/slenium/Demo.java
+++ Learn_Selenium/src/test/java/slenium/Demo.java
@@ -2,8 +2,38 @@ package slenium;
 
 public class Demo {
 
-	public static void main(String[] args) {
-		System.out.println("Hey Tester, Hiiii....");
+	public static void main(String[] args) throws InterruptedException {
+
+	System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Chrome_driver\\chromedriver.exe");
+
+	WebDriver driver=new ChromeDriver();
+
+	driver.get("http://demowebshop.tricentis.com/login");
+
+	driver.manage().window().maximize();
+
+	driver.findElement(By.xpath("//input[@value='Register']")).click();
+
+	driver.navigate().back();
+
+	Thread.sleep(3000);
+
+	driver.navigate().forward();
+
+	Thread.sleep(3000);
+
+	driver.navigate().to("http://demowebshop.tricentis.com/login");
+
+	Thread.sleep(3000);
+
+	driver.navigate().refresh();
+
+	Thread.sleep(3000);
+
+	driver.close();
+
+
+
 
 	}
 
